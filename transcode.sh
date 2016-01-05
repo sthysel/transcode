53219d1ae7a3e6af95405ca84a13441e781a3f61
Thys Meintjes <tmeintjes@ccg.murdoch.edu.au>
2016-01-05 18:30:49 +0800

    Init

---
+++ transcode.sh
@@ -0,0 +1,33 @@
+#!/bin/sh                                                                                                                                                     
+
+set -e
+
+readonly VCODEC="mp4v"
+readonly VBITRATE="1024"
+
+readonly ACODEC="mp4a"
+readonly ABITRATE="128"
+
+readonly MUX="mp4"
+
+readonly VLC="/usr/bin/vlc"                                                                                                                                           
+
+if [ ! -e "${VLC}" ]
+then
+    echo "'${VLC}' not available"
+    exit 1
+fi
+
+for file in "$@"
+do
+    echo "=> Transcoding '${file}'... "
+
+    dst=$(dirname "${file}")
+    new=$(basename "${file}" | sed 's@\.[a-z][a-z][a-z]$@@').${MUX}
+
+    ${VLC} -I dummy -q "${file}" \
+       --sout "#transcode{vcodec=${VCODEC},vb=${VBITRATE},acodec=${ACODEC},ab=${ABITRATE}}:standard{mux=${MUX},dst=\"${dst}/${new}\",access=file}" \
+       vlc://quit
+    ls -lh "${file}" "${dst}/$new"
+    echo
+done
