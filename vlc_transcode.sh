1f4753115da632ae603fefba7ab48af7970de11d
Thys Meintjes <tmeintjes@ccg.murdoch.edu.au>
2016-01-05 20:13:56 +0800

    renamed

--- vlc_transcode.sh
+++ vlc_transcode.sh
@@ -2,13 +2,15 @@
 
 set -e
 
+readonly ALLFILES=$@
+
 readonly VCODEC="mp4v"
-readonly VBITRATE="1024"
+readonly VBITRATE="512"
 
 readonly ACODEC="mp4a"
 readonly ABITRATE="128"
 
-readonly MUX="mp4" 
+readonly MUX="ogg" 
 readonly VLC="/usr/bin/vlc"                                                                                                                                           
 
 if [ ! -e "${VLC}" ]
@@ -17,23 +19,26 @@ then
     exit 1
 fi
 
+# https://www.videolan.org/doc/streaming-howto/en/ch03.html
 transcode() {
-    for file in "$@"
+    for file in ${ALLFILES}
     do
-	echo "=> Transcoding '${file}'... "
+	echo "Transcoding '${file}'... "
 
 	dst=$(dirname "${file}")
 	new=$(basename "${file}" | sed 's@\.[a-z][a-z][a-z]$@@').${MUX}
 
 	${VLC} -I dummy -q "${file}" \
-	--sout "#transcode{vcodec=${VCODEC},vb=${VBITRATE},acodec=${ACODEC},ab=${ABITRATE}}:standard{mux=${MUX},dst=\"${dst}/${new}\",access=file}" \
+	--sout "#\
+	transcode{vcodec=${VCODEC},venc=ffmpeg,vb=${VBITRATE},acodec=${ACODEC},ab=${ABITRATE}}:\
+	standard{mux=${MUX},dst=\"${dst}/${new}\",access=file}" \
 	vlc://quit
 	ls -lh "${file}" "${dst}/$new"
 	echo
     done
 }
 
-if [ -z "$@" ]
+if [ -z ${ALLFILES} ]
 then
     echo "$(basename $0) *.ASF"
     exit 1
